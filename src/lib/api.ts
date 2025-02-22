261b3c22a6c4028c0d0f7c6cf1940582bc16072a
Keyru Nasir <keyrsyntax@gmail.com>
2025-02-22 18:13:15 +0300

    backend URL added

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  baseURL:"https://www.backend-internship-project.keyrunasir.com",
   withCredentials: true,
 });
 
