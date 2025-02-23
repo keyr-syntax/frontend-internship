d918e2e437d82b0d9031a33a0e84a3c0a1f697eb
Keyru Nasir <keyrsyntax@gmail.com>
2025-02-23 08:44:51 +0300

    final commit

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = axios.create({
-  // baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:8000",
   baseURL: "https://www.backend-internship-project.keyrunasir.com",
   withCredentials: true,
 });
