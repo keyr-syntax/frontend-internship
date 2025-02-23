6f3a59883661b94a99eb697c5b97ee1539c2ae18
Yeabsira Tarekegn <99181424+itsyaba@users.noreply.github.com>
2025-02-23 10:26:36 +0300

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = axios.create({
   // baseURL: "http://localhost:8000",
-  baseURL: "https://www.backend-internship-project.keyrunasir.com",
+  baseURL: "https://intern-project-backend-43ao.vercel.app",
   withCredentials: true,
 });
 
